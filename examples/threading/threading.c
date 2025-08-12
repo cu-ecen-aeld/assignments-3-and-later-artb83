958e98edc3aec2f698b9e80c114df7f7128a615f
artb83 <21041434+artb83@users.noreply.github.com>
2025-08-12 17:21:23 +0300

    Working version of threading after debug. Found  **pointer of thread_func argument passed to pthread_create instead of *pointer.

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -4,32 +4,39 @@
 #include <stdio.h>
 
 // Optional: use these functions to add debug or error prints to your application
-#define DEBUG_LOG(msg,...)
-//#define DEBUG_LOG(msg,...) printf("threading: " msg "\n" , ##__VA_ARGS__)
+//#define DEBUG_LOG(msg,...)
+#define DEBUG_LOG(msg,...) printf("threading: " msg "\n" , ##__VA_ARGS__)
 #define ERROR_LOG(msg,...) printf("threading ERROR: " msg "\n" , ##__VA_ARGS__)
+
 
 void* threadfunc(void* thread_param)
 {
     // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
-	DEBUG_LOG("\nInside thread function\n");
+	//struct thread_data* thread_func_args = (struct thread_data *) thread_param;
 	struct thread_data* thread_func_args = (struct thread_data*)thread_param;
+	DEBUG_LOG("Thread started\n");
 
 	usleep(thread_func_args->m_wait_to_obtain_ms);
-	if(pthread_mutex_lock(thread_func_args->m_mutex)!=0) return thread_param;
 
-	usleep(thread_func_args->m_wait_to_release_ms);
-	if(pthread_mutex_unlock(thread_func_args->m_mutex)!=0) return thread_param;
+	if(pthread_mutex_lock(thread_func_args->m_mutex)!=0){
+		DEBUG_LOG("Could not obtain lock\n");
+		return thread_param;
+	}
+	DEBUG_LOG("Thread critical section");
 
 	thread_func_args->thread_complete_success=true;
-	DEBUG_LOG("Thread function complete");
+	usleep(thread_func_args->m_wait_to_release_ms);
+	pthread_mutex_unlock(thread_func_args->m_mutex);
+
+	DEBUG_LOG("Thread complete\n");
     return thread_param;
 }
 
 
 bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int wait_to_obtain_ms, int wait_to_release_ms)
 {
-	DEBUG_LOG("Inside thread starter function: start_thread_obtaining_mutex\n");
+	DEBUG_LOG("Thread starter function\n");
     /**
      * TODO: allocate memory for thread_data, setup mutex and wait arguments, pass thread_data to created thread
      * using threadfunc() as entry point.
@@ -39,7 +46,10 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
      * See implementation details in threading.h file comment block
      */
 	bool bSuccess = true;
-	struct thread_data* thread_func_args = (struct thread_data*)malloc(sizeof(struct thread_data));
+	struct thread_data* thread_func_args = (struct thread_data*)calloc(1, sizeof(struct thread_data));
+	if(!thread_func_args){
+		return false;
+	}
 
 	thread_func_args->thread_complete_success=false;
 	thread_func_args->m_wait_to_obtain_ms=wait_to_obtain_ms;
@@ -51,39 +61,8 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
 		free(thread_func_args);
 		return false;
 	}
-//	if(!thread_func_args->thread_complete_success){
-//		pthread_detach(*thread);
-//	} else{
-//		void* res;
-//		pthread_join(*thread, &res);
-//		//printf("Joined with thread %ld; returned value was %c\n", *thread, *(char *)res);
-//	}
-	bSuccess = thread_func_args->thread_complete_success;
-	if(bSuccess) DEBUG_LOG("Threading OK");
-	free(thread_func_args);
+	if(thread_func_args->thread_complete_success){
+		bSuccess=true;
+	}
     return bSuccess;
 }
-
-//int main(){
-//	printf("Main Started\n");
-//	bool thread_complete_success=false;
-//	int wait_to_obtain_ms=1000;
-//	int wait_to_release_ms=1000;
-//	struct thread_data thread_func_args;
-//	pthread_mutex_t mutex;
-//	pthread_t thread;
-//	thread_func_args.m_wait_to_obtain_ms=wait_to_obtain_ms;
-//	thread_func_args.m_wait_to_release_ms=wait_to_release_ms;
-//	thread_func_args.thread_complete_success = false;
-//	pthread_mutex_init(&mutex, NULL);
-//	if(0!=pthread_create(&thread, NULL, &threadfunc, &thread_func_args)){
-//			ERROR_LOG("Failed to start a thread");
-//			return false;
-//	}
-//	//thread_complete_success=start_thread_obtaining_mutex(&thread, &mutex, wait_to_obtain_ms, wait_to_release_ms);
-//	pthread_join(thread, NULL);
-//	if(thread_complete_success) DEBUG_LOG("Main OK");
-//	else ERROR_LOG("Main Failed");
-//	pthread_mutex_destroy(&mutex);
-//	return 0;
-//}
